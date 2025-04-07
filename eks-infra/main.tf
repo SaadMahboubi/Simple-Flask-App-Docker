b883952a2c6209380dd12a878b06503357fb410a
SaadMahboubi <saad.mahboubi.info@gmail.com>
2025-04-07 20:11:04 +0200

    adding terraform deployement

---
+++ eks-infra/main.tf
@@ -0,0 +1,46 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "eu-west-3a"
+}
+
+resource "aws_subnet" "subnet_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "eu-west-3b"
+}
+
+resource "aws_eks_cluster" "this" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_cluster.arn
+
+  vpc_config {
+    subnet_ids = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks_AmazonEKSClusterPolicy]
+}
+
+resource "aws_iam_role" "eks_cluster" {
+  name = "eksClusterRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_AmazonEKSClusterPolicy" {
+  role       = aws_iam_role.eks_cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
---
+++ eks-infra/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  default = "eu-west-3"
+}
+
+variable "cluster_name" {
+  default = "kube-shop-cluster"
+}
+
+variable "node_instance_type" {
+  default = "t3.medium"
+}
